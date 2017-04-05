29c77330a81aff7ac6084a66fa331e93f738c224
Mike Sciking <sciking@users.noreply.github.com>
2017-04-05 20:53:51 +0200

    Create prot.py

---
+++ prot.py
@@ -0,0 +1,24 @@
+#PROT: PROGRAMMAZIONE RADICALMENTE ORGANIZZATA TECNICA
+ans = 0
+lan = -1
+it = 0
+a = 0
+b = 0
+m = 0
+r = 0
+#funzioni semplici [a primo arg]
+def sadd(x):
+	ans = a + x
+def sdiff(x):
+	ans = a - x
+def sper(x):
+	ans = a *x
+def sdiv(x):
+	ans = a /x
+	r = a%x
+def sif(x):
+	if x =>0:
+		return 1
+	else:
+		return 0
+	
